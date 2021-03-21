60f069d9dc53cd5abc09cf4a6f6f0e22da05d6d5
Alexgub84 <alex.gub@gmail.com>
2021-03-21 22:12:08 +0200

    Updates

---
+++ precache-manifest.298031d87cee2145182ae0c2700c70b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2ab55e05fca4fc59253823dddc3def",
+    "url": "/misterbit-test/index.html"
+  },
+  {
+    "revision": "d35dbb63769f1f0c6ad0",
+    "url": "/misterbit-test/static/css/main.2cf33c9e.chunk.css"
+  },
+  {
+    "revision": "6ad16ff2004ad4503b3e",
+    "url": "/misterbit-test/static/js/2.13213aaf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/misterbit-test/static/js/2.13213aaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d35dbb63769f1f0c6ad0",
+    "url": "/misterbit-test/static/js/main.7f3f3796.chunk.js"
+  },
+  {
+    "revision": "f4085fde6891c873e6b6",
+    "url": "/misterbit-test/static/js/runtime-main.d4e14c2b.js"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/misterbit-test/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/misterbit-test/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
